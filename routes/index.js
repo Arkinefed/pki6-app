b80dfca43b2f9bf4420411101e811a0d86f76546
Arkinefed <arkinefedmail@gmail.com>
2023-04-10 18:31:45 +0200

    inst8 - etap 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-	//res.render('index', { title: 'Express' });
-	res.send('Hello world');
+const { google } = require('googleapis');
+const OAuth2Data = require('../google_auth.json')
+
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+var authed = false;
+
+router.get('/', (req, res) => {
+	if (!authed) {
+		// Generate an OAuth URL and redirect there
+		const url = oAuth2Client.generateAuthUrl({
+			access_type: 'offline',
+			scope: 'https://www.googleapis.com/auth/gmail.readonly'
+		});
+		console.log(url)
+		res.redirect(url);
+	} else {
+		res.send('Logged in')
+	}
+})
+
+router.get('/auth/google/callback', function (req, res) {
+	const code = req.query.code
+	if (code) {
+		// Get an access token based on our OAuth code
+		oAuth2Client.getToken(code, function (err, tokens) {
+			if (err) {
+				console.log('Error authenticating')
+				console.log(err);
+			} else {
+				console.log('Successfully authenticated');
+				oAuth2Client.setCredentials(tokens);
+				authed = true;
+				res.redirect('/')
+			}
+		});
+	}
 });
 
 module.exports = router;
